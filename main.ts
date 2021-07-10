512e8598847e5cc36c215334ac80c431716e4d99
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2021-07-10 20:36:36 +0100

    tidy prior to sharing

--- main.ts
+++ main.ts
@@ -3,11 +3,7 @@ input.onButtonPressed(Button.A, function () {
     basic.showNumber(Rtraq)
     dfplayermini.playFile(Rtraq, dfplayermini.isRepeat.No)
 })
-input.onButtonPressed(Button.B, function () {
-    dfplayermini.press(dfplayermini.playType.PlayNext)
-})
 let Rtraq = 0
 dfplayermini.connect(SerialPin.P0, SerialPin.P1)
 dfplayermini.setVolume(17)
-dfplayermini.setEQ(dfplayermini.EQ.Normal)
 basic.showIcon(IconNames.Yes)
